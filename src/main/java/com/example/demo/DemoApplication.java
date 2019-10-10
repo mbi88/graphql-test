5c6eaf6fdafb8a481697a8b702136c643e5985b6
Maksym Binkovskyi <mb@audienceproject.com>
2019-10-10 18:19:26 +0300

    init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import com.example.demo.model.Author;
+import com.example.demo.model.Book;
+import com.example.demo.repository.AuthorRepository;
+import com.example.demo.repository.BookRepository;
+import com.example.demo.resolver.BookResolver;
+import com.example.demo.resolver.Mutation;
+import com.example.demo.resolver.Query;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public BookResolver authorResolver(AuthorRepository authorRepository) {
+        return new BookResolver(authorRepository);
+    }
+
+    @Bean
+    public Query query(AuthorRepository authorRepository, BookRepository bookRepository) {
+        return new Query(authorRepository, bookRepository);
+    }
+
+    @Bean
+    public Mutation mutation(AuthorRepository authorRepository, BookRepository bookRepository) {
+        return new Mutation(authorRepository, bookRepository);
+    }
+
+    @Bean
+    public CommandLineRunner demo(AuthorRepository authorRepository, BookRepository bookRepository) {
+        return (args) -> {
+            Author author = new Author("Herbert", "Schildt");
+            authorRepository.save(author);
+
+            bookRepository.save(new Book("Java: A Beginner's Guide, Sixth Edition", "0071809252", 728, author));
+        };
+    }
+}
